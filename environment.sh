6747e3424afabc29d725a5b45f7d1b8314f736dc
Matthew Brown <matt@ederoyd.co.uk>
2013-09-21 23:36:35 +0100

    removed database installs

--- environment.sh
+++ environment.sh
@@ -14,19 +14,5 @@ process_owner=`whoami`
 haskell_install=$platform/osmimport
 haskell_bin=$haskell_install/bin
 
-#MongoDB
-mongo_install=$platform/mongo
-mongo_data=$platform/data/mongo
-mongo_bin=$platform/mongo/bin
-mongo_port=7720
-mongo_version=2.4.6
-
-#Redis
-redis=$platform/redis
-redis_version=2.6.16
-redis_bin=$redis
-redis_port=7721
-redis_data=$platform/data/redis
-
-export PATH=$haskell_bin:$mongo_bin:$redis_bin:$PATH
+export PATH=$haskell_bin:$PATH
 
